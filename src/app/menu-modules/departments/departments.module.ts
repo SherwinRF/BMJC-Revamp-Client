7a72e5cf034543313550f00a7350b94b9a856763
saurabhjdas <saurabh_das@persistent.com>
2021-01-12 19:55:43 +0530

    Update Departments and Nav-bar

--- src/app/menu-modules/departments/departments.module.ts
+++ src/app/menu-modules/departments/departments.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from "@angular/common";
 import { DepartmentsRoutingModule } from "./departments-routing.module";
 import { DepartmentsComponent } from "./departments/departments.component";
 import { HttpClientModule } from "@angular/common/http";
+import { LoadingModule} from "../../loading-module/loading/loading.module"
 
 @NgModule({
   declarations: [DepartmentsComponent],
-  imports: [CommonModule, DepartmentsRoutingModule, HttpClientModule],
+  imports: [CommonModule, DepartmentsRoutingModule, HttpClientModule, LoadingModule ],
 })
 export class DepartmentsModule {}
